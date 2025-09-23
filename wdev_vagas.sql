2b0918865825edadfdf958d70d3011232cceea2d
JoaoVictorCavalcante <joaovictorribeiro921@gmail.com>
2025-09-23 15:47:45 -0300

    Projeto 23/09 v2

--- wdev_vagas.sql
+++ wdev_vagas.sql
@@ -1,73 +1,38 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Tempo de geração: 20/08/2025 às 16:41
--- Versão do servidor: 8.0.39-0ubuntu0.24.04.2
--- Versão do PHP: 8.3.6
+drop table vagas;
+create table vagas(
+    id int auto_increment primary key,
+    titulo varchar(100),
+    descricao text,
+    quantidade int,
+    remuneracao decimal(7,2),
+    data_abertura date,
+    data_fechamento date,
+    data_criacao timestamp
+);
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+create table alunos(
+    id int auto_increment primary key,
+    nome varchar(255),
+    cpf varchar(255) unique, 
+    telefone varchar(255),
+    email_institucional varchar(255) unique,
+    email_pessoal varchar(255),
+    matricula varchar(255) unique,
+    curso varchar(255),
+    periodo varchar(2),
+    dtn date,
+    nivel int
+);
 
+insert into alunos (nome, cpf, telefone, email_institucional, email_pessoal, matricula, curso, periodo, dtn, nivel)
+values ('Admin','00000000000','0000','admin@ifto.edu.br','admin@gmail.com','000','Tecnologia da Informação','8','2001-01-01',2);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `wdev_vagas`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `vagas`
---
-
-CREATE TABLE `vagas` (
-  `id` int NOT NULL,
-  `titulo` varchar(255) NOT NULL,
-  `descricao` text NOT NULL,
-  `ativo` enum('s','n') NOT NULL,
-  `data` timestamp NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
---
--- Despejando dados para a tabela `vagas`
---
-
-INSERT INTO `vagas` (`id`, `titulo`, `descricao`, `ativo`, `data`) VALUES
-(1, 'eeee', 'eeee', 's', '2025-08-12 21:49:15'),
-(2, 'dddd', 'dddddd', 's', '2025-08-12 21:49:18'),
-(3, '', '', 's', '2025-08-12 21:49:19'),
-
-
---
--- Índices para tabelas despejadas
---
-
---
--- Índices de tabela `vagas`
---
-ALTER TABLE `vagas`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT para tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `vagas`
---
-ALTER TABLE `vagas`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-CREATE Table 
+create table inscricao(
+    id int auto_increment primary key,
+    id_vaga int,
+    id_aluno int,
+    data_inscricao timestamp,
+    status varchar(50),
+    foreign key (id_vaga) references vagas (id),
+    foreign key (id_aluno) references alunos (id)
+);
